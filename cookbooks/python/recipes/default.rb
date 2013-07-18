7a3da719cd69951b1d6f128187cbfd9849596773
Kisitu Augustine <austiine04@gmail.com>
2013-07-18 11:24:58 +0300

    changing python cookbook to install python2.7

--- cookbooks/python/recipes/default.rb
+++ cookbooks/python/recipes/default.rb
@@ -1,32 +1,8 @@
-#
-# Cookbook Name:: python
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 execute "installing updates" do
   command "sudo apt-get update"
   action :run
 end
 
-package "python-software-properties" do 
-  action :install
-end
-
-execute "adding the ppa for python2.6" do
-  command "sudo add-apt-repository ppa:fkrull/deadsnakes"
-  action :run
-end
-
-execute "installing updates" do
-  command "sudo apt-get update"
-  action :run
-end
-
-# %w{build-essential python-setuptools python2.6 python2.6-dev python-pip libpq-dev libxml2 libxml2-dev libxslt1-dev}.each do |pkg|
 %w{build-essential python-setuptools python-dev python-pip libpq-dev libxml2 libxml2-dev libxslt1-dev}.each do |pkg|
   package pkg do
     action :install
@@ -40,7 +16,6 @@ end
 
 execute "creating edtrac virtualenv" do
   cwd "/home/vagrant"
-  # command "virtualenv edtrac_env --python=python2.6"
   command "virtualenv edtrac_env"
   action :run
 end
