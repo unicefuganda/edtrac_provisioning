ea808c1e69943a591c831b154bae6694dda0f83a
Kisitu Augustine <austiine04@gmail.com>
2013-07-18 15:47:28 +0300

    cleaned up edtrac cookbook

--- cookbooks/edtrac/recipes/default.rb
+++ cookbooks/edtrac/recipes/default.rb
@@ -1,47 +1,21 @@
-#
-# Cookbook Name:: edtrac
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
- 
-git "/home/vagrant/edtrac" do
-    repository "https://github.com/unicefuganda/edtrac.git"
-    reference "master"
-    action :checkout
-end
-
-execute "initialize submodules" do
-  cwd "/home/vagrant/edtrac"
-  command "git submodule init"
-  action :run
-end
-
-execute "update all the submodules" do
-  cwd "/home/vagrant/edtrac"
-  command "git submodule update"
-  action :run
-end
-
 execute "activating virtualenv and installing dependencies" do
-  cwd "/home/vagrant/edtrac"
+  cwd "/vagrant"
   command "bash -c 'source /home/vagrant/edtrac_env/bin/activate && pip install -r requirements.pip'"
   action :run
 end
 
-template "/home/vagrant/edtrac/edtrac_project/localsettings.py" do
+template "/vagrant/edtrac_project/localsettings.py" do
   source "localsettings.py.erb"
 end
 
 execute "running migrations" do
-  cwd "/home/vagrant/edtrac/edtrac_project"
+  cwd "/vagrant/edtrac_project"
   command "bash -c 'source /home/vagrant/edtrac_env/bin/activate && python manage.py syncdb --noinput && python manage.py migrate'"
   action :run
 end
 
 execute "setup the initial data in the database" do
-  cwd "/home/vagrant/edtrac/edtrac_project"
+  cwd "/vagrant/edtrac_project"
   command "bash -c 'source /home/vagrant/edtrac_env/bin/activate && python manage.py initialize_database'"
   action :run
 end
